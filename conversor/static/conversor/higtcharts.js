e210286bb264277b68d46fd4c754a8cce6e4080e
Messias Ferreira <messiaszrj@hotmail.com>
2023-09-21 20:15:49 -0300

    projeto funcionando

--- conversor/static/conversor/higtcharts.js
+++ conversor/static/conversor/higtcharts.js
@@ -1,125 +1,154 @@
-// A point click event that uses the Renderer to draw a label next to the point
-// On subsequent clicks, move the existing label instead of creating a new one.
-Highcharts.addEvent(Highcharts.Point, 'click', function () {
-    if (this.series.options.className.indexOf('popup-on-click') !== -1) {
-        const chart = this.series.chart;
-        const date = Highcharts.dateFormat('%A, %b %e, %Y', this.x);
-        const text = `<b>${date}</b><br/>${this.y} ${this.series.name}`;
+function load_data_init() {
 
-        const anchorX = this.plotX + this.series.xAxis.pos;
-        const anchorY = this.plotY + this.series.yAxis.pos;
-        const align = anchorX < chart.chartWidth - 200 ? 'left' : 'right';
-        const x = align === 'left' ? anchorX + 10 : anchorX - 10;
-        const y = anchorY - 30;
-        if (!chart.sticky) {
-            chart.sticky = chart.renderer
-                .label(text, x, y, 'callout',  anchorX, anchorY)
-                .attr({
-                    align,
-                    fill: 'rgba(0, 0, 0, 0.75)',
-                    padding: 10,
-                    zIndex: 7 // Above series, below tooltip
-                })
-                .css({
-                    color: 'white'
-                })
-                .on('click', function () {
-                    chart.sticky = chart.sticky.destroy();
-                })
-                .add();
-        } else {
-            chart.sticky
-                .attr({ align, text })
-                .animate({ anchorX, anchorY, x, y }, { duration: 250 });
+    let moeda = $("#moeda").children("option:selected").val()
+
+    axios.get(
+        '/consulta',
+        {
+            params: {
+                "moeda": moeda
+            }
         }
-    }
-});
+    )
+        .then((response) => {
+
+            let data = response.data
+
+            let categorias = new Array()
+            let cotacaoes = new Array()
+
+            data.forEach(element => {
+                categorias.push(
+                    element.data
+                )
+                cotacaoes.push(element.cotacao)
+            });
+
+            chart.xAxis[0].setCategories(Array.from(categorias))
+            chart.addSeries({
+                name: moeda,
+                data: cotacaoes
+            })
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 
 
-Highcharts.chart('container', {
-
-    chart: {
-        scrollablePlotArea: {
-            minWidth: 700
-        }
-    },
+const chart = Highcharts.chart('container', {
 
     title: {
-        text: 'Daily sessions at www.highcharts.com',
+        text: "Cotação do USD",
         align: 'left'
+    },
+
+    events: {
+        load: load_data_init()
     },
 
     subtitle: {
-        text: 'Source: Google Analytics',
+        text: 'By Vat Comply. Source: <a href="https://www.vatcomply.com/" target="_blank">VAT</a>.',
         align: 'left'
     },
 
-    xAxis: {
-        tickInterval: 7 * 24 * 3600 * 1000, // one week
-        tickWidth: 0,
-        gridLineWidth: 1,
-        labels: {
-            align: 'left',
-            x: 3,
-            y: -3
+    yAxis: {
+        title: {
+            text: 'Valor da Moeda'
         }
     },
 
-    yAxis: [{ // left y axis
-        title: {
-            text: null
-        },
-        labels: {
-            align: 'left',
-            x: 3,
-            y: 16,
-            format: '{value:.,0f}'
-        },
-        showFirstLabel: false
-    }, { // right y axis
-        linkedTo: 0,
-        gridLineWidth: 0,
-        opposite: true,
-        title: {
-            text: null
-        },
-        labels: {
-            align: 'right',
-            x: -3,
-            y: 16,
-            format: '{value:.,0f}'
-        },
-        showFirstLabel: false
-    }],
+    xAxis: {
+        categories: [],
+    },
 
     legend: {
-        align: 'left',
-        verticalAlign: 'top',
-        borderWidth: 0
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
     },
 
-    tooltip: {
-        shared: true,
-        crosshairs: true
-    },
+    series: null,
 
-    plotOptions: {
-        series: {
-            cursor: 'pointer',
-            className: 'popup-on-click',
-            marker: {
-                lineWidth: 1
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 1000
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    chart
+});
+
+
+
+
+$("#submit").on("click", () => {
+    let dt_inicio = $("#dataInicio").val()
+    let dt_fim = $("#dataFim").val()
+    let moeda = $("#moeda").children("option:selected").val()
+
+    data_inicio = new Date(dt_inicio)
+    data_fim = new Date(dt_fim)
+    if (dt_inicio != "" && dt_fim != "") {
+        let diff = Math.abs(data_inicio.getTime() - data_fim.getTime());
+        let diffDays = Math.ceil(diff / (1000 * 3600 * 24)); 
+
+        if (diffDays > 5) {
+            alert("Escolher período no maximo de 5 dias ")
+            return
+        }
+    }
+
+    if (dt_inicio == "" || dt_fim == ""){
+        alert("Preencha os dois campos datas")
+    }
+
+    axios.get(
+        '/consulta',
+        {
+            params: {
+                "dt_inicio": dt_inicio,
+                "dt_fim": dt_fim,
+                "moeda": moeda
             }
         }
-    },
+    )
+        .then((response) => {
+            chart.series[0].remove()
+            chart.xAxis[0].categories = []
+            let data = response.data
 
-    series: [{
-        yAxis: 0,
-        name:"Messias",
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-    },{
-        yAxis: 1,
-        name:   "Teste",
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }]
-});
+            let categorias = new Array()
+            let cotacaoes = new Array()
+
+            data.forEach(element => {
+                categorias.push(
+                    element.data
+                )
+                cotacaoes.push(element.cotacao)
+            });
+
+            chart.xAxis[0].setCategories(Array.from(categorias))
+            chart.addSeries({
+                name: moeda,
+                data: cotacaoes
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+})
--- conversor/views.py
+++ conversor/views.py
@@ -4,6 +4,7 @@ Modulo view para acesso dos processos
 from django.shortcuts import render
 from rest_framework import generics
 
+from django.http.response import JsonResponse
 from conversor.serializer import CotacaoSerialize
 from conversor.models import Cotacao
 from conversor.utils import converter_data
@@ -27,14 +28,16 @@ class CotacoesList(generics.ListAPIView):
 
         dt_inicio = self.request.query_params.get('dt_inicio')
         dt_fim = self.request.query_params.get('dt_fim')
+        moeda = self.request.query_params.get('moeda', "EUR")
 
-        resultado = Cotacao.objects.all()
+        resultado = Cotacao.objects.filter(moeda=moeda)
 
         if dt_inicio and dt_fim:
-            resultado = resultado.filter(data__gte=converter_data(dt_inicio), data__lte=converter_data(dt_fim))
+            resultado = resultado.filter(data__gte=converter_data(dt_inicio), data__lte=converter_data(dt_fim), moeda__in=["EUR", "JPY", "BRL"])
         elif dt_inicio:
-            resultado = resultado.filter(data__gte=converter_data(dt_inicio))
+            resultado = resultado.filter(data__gte=converter_data(dt_inicio), moeda__in=["EUR", "JPY", "BRL"])
         elif dt_fim:
-            resultado = resultado.filter(data__lte=converter_data(dt_inicio))
+            resultado = resultado.filter(data__lte=converter_data(dt_inicio), moeda__in=["EUR", "JPY", "BRL"])
+        
+        return resultado.order_by("data")[:5]
 
-        return resultado
